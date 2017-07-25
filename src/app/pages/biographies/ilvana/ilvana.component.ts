e8cb6cf6c41b86eeb774664a86b2aa73714214e9
Lonara <edin.lonic@edu.fit.ba>
2017-07-25 21:13:54 +0200

    fixed image paths

--- src/app/pages/biographies/ilvana/ilvana.component.ts
+++ src/app/pages/biographies/ilvana/ilvana.component.ts
@@ -1,18 +1,9 @@
 import { Component, Input } from '@angular/core';
-declare var $:any;
 @Component({
     selector: "ilvana",
     templateUrl: 'ilvana.component.html',
 })
 export class IlvanaComponent {
 
-
-ngAfterViewInit(){
-           $(document).ready(function(){
-             $(".bio-link").click(function(){
-             $(this).hide();
-             });
-           });
-     }
 
 }
